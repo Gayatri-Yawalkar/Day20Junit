d9ea0892183671dd4766c8bb4cfbd570694b5ac0
unknown <gayoyawalkar@gmail.com>
2021-10-19 06:42:33 +0530

    Welcome to User Registration with Test Case

---
+++ Hello.java
@@ -0,0 +1,6 @@
+package com.bridgelabz.bank;
+public class Hello {
+	public static void main(String[] args) {
+		System.out.println("Welcome to User Registration Program with Junit Test Cases");
+	}
+}
